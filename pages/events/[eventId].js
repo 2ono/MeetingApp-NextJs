9d6914e187af34b61f484ed25b83b020ebc7d89a
2ono <clt3555@gmail.com>
2023-09-03 11:28:32 +0900

    [add] Event Detail Page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,31 @@
+import { useRouter } from "next/router";
+import { getEventById } from "@/dummy-data";
+import { Fragment } from "react";
+import EventSummary from "@/components/event-detail/event-summary";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventContent from "@/components/event-detail/event-content";
+
 function EventDetailsPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
   return (
-    <div>
-      <h1>Event Detail</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} image={event.image} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.imageAlt}
+      />
+      <EventContent test={event.description}>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 }
 
